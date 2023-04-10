099d2951b980fd04f810c083515c3f5f0d15d51d
Naveen Bhatti <jrbhrn@gmail.com>
2023-04-09 20:10:24 -0500

    Add simple navbar and new visitor page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">United Christian Church</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link" to="/new">I'M NEW</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/sermons">SERMONS</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">ABOUT</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/connect">CONNECT</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/ministries">MINISTRIES</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/calendar">CALENDAR</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact">CONTACT</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   )
 }
 
